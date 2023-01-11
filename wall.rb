28b5e36438b8f4fa9230c7a26c2f6e5b174f1c2b
NgDucLoc <ngducloc1112002@gmail.com>
2023-01-11 12:43:13 +0700

    1

--- wall.rb
+++ wall.rb
@@ -41,8 +41,8 @@ class Wall
   def hitUp(pacmanX , pacmanY)
     if  ((pacmanY + 45) == @y) &&
         ( (pacmanX).between?( (@x-@a) , (@x+@height+@a) )   || 
-          (pacmanX+50).between?( (@x-@a), (@x+@height+@a) ) ||
-          (pacmanX+25).between?( (@x-@a) , (@x+@height+@a) ) ) &&
+          (pacmanX+40).between?( (@x-@a), (@x+@height+@a) ) ||
+          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) ) &&
         (@through == false)  
       return true
     else
@@ -53,8 +53,8 @@ class Wall
   def hitDown(pacmanX , pacmanY)
     if  ((pacmanY +5 ) == @y+@width) &&
         ( (pacmanX).between?( (@x-@a) , (@x+@height+@a) )   ||
-          (pacmanX+50).between?( (@x-@a), (@x+@height+@a) ) ||
-          (pacmanX+25).between?( (@x-@a) , (@x+@height+@a) ) ) &&
+          (pacmanX+40).between?( (@x-@a), (@x+@height+@a) ) ||
+          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) ) &&
         (@through == false)
       return true
     else
@@ -65,8 +65,8 @@ class Wall
   def hitLeft(pacmanX , pacmanY)
     if  ((pacmanX + 45 ) == @x) &&
         ( (pacmanY).between?( (@y-@a) , (@y+@width+@a) )   || 
-          (pacmanY+50).between?( (@y-@a), (@y+@width+@a) ) ||
-          (pacmanY+25).between?( (@y-@a) , (@y+@width+@a) ) ) &&
+          (pacmanY+40).between?( (@y-@a), (@y+@width+@a) ) ||
+          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) ) &&
       (@through == false)
       return true
     else
@@ -77,8 +77,8 @@ class Wall
   def hitRight(pacmanX , pacmanY)
     if  ((pacmanX + 5) == @x+@height) &&
         ( (pacmanY).between?( (@y-@a) , (@y+@width+@a) )   || 
-          (pacmanY+50).between?( (@y-@a), (@y+@width+@a) ) ||
-          (pacmanY+25).between?( (@y-@a) , (@y+@width+@a) ) ) &&
+          (pacmanY+40).between?( (@y-@a), (@y+@width+@a) ) ||
+          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) ) &&
         (@through == false)
       return true
     else
