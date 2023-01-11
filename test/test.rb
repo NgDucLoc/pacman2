5b937979ba2d5a8d1c72dc4a8fea5a3f26f8a798
NgDucLoc <ngducloc1112002@gmail.com>
2023-01-11 14:35:59 +0700

    .

--- test/test.rb
+++ test/test.rb
@@ -27,6 +27,9 @@ class Wall
         @height = h
         @width = w
         @img = $walls_file[axis]
+
+        puts @x
+        puts @y
         if axis <21
             @through = false
         else 
@@ -57,9 +60,7 @@ class Window < Gosu::Window
         t=0
         for i in 0..$wallsdoc.length-1
             row =  $wallsdoc[i]
-            puts row
             for j in 0..row.length-1
-                puts j
                 $walls[t] =Wall.new(SIZE*i,SIZE*j, SIZE, SIZE, $wallsdoc[i][j].to_i) 
                 t=t+1
             end
@@ -74,4 +75,5 @@ class Window < Gosu::Window
         end
     end
 end
+
 Window.new.show
--- wall.rb
+++ wall.rb
@@ -29,9 +29,8 @@ class Wall
       else 
           @through = false
       end
-      puts @through
       @image = Gosu::Image.new(@img , options = {:rect => [0, 0, @height, @width]} )
-      @a = -10
+      @a = 1
   end
   
   def draw
@@ -39,48 +38,52 @@ class Wall
   end
 
   def hitUp(pacmanX , pacmanY)
-    if  ((pacmanY + 38) == @y) &&
-        ( (pacmanX).between?( (@x-@a) , (@x+@height+@a) )   || 
-          (pacmanX+40).between?( (@x-@a), (@x+@height+@a) ) ||
-          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) ) &&
-        (@through == false)  
-      return true
+    if (@through == false) 
+      if  ((pacmanY + 40) == @y) &&
+         ((pacmanX+9).between?( (@x-@a) , (@x+@height+@a) )   || 
+          (pacmanX+31).between?( (@x-@a), (@x+@height+@a) ) ||
+          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) )   
+        return true
+      end
     else
       return false
     end
   end
 
   def hitDown(pacmanX , pacmanY)
-    if  ((pacmanY +2 ) == @y+@width) &&
-        ( (pacmanX).between?( (@x-@a) , (@x+@height+@a) )   ||
-          (pacmanX+40).between?( (@x-@a), (@x+@height+@a) ) ||
-          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) ) &&
-        (@through == false)
-      return true
+    if (@through == false)
+      if  ((pacmanY + 5 ) == @y+@width) &&
+        ( (pacmanX +9).between?( (@x-@a) , (@x+@height+@a) )   ||
+          (pacmanX+31).between?( (@x-@a), (@x+@height+@a) ) ||
+          (pacmanX+20).between?( (@x-@a) , (@x+@height+@a) ) ) 
+        return true
+      end
     else
-    return false
+      return false
     end
   end
 
   def hitLeft(pacmanX , pacmanY)
-    if  ((pacmanX + 38 ) == @x) &&
-        ( (pacmanY).between?( (@y-@a) , (@y+@width+@a) )   || 
-          (pacmanY+40).between?( (@y-@a), (@y+@width+@a) ) ||
-          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) ) &&
-      (@through == false)
-      return true
+    if (@through == false)
+      if  ((pacmanX + 40 ) == @x) &&
+        ( (pacmanY+9).between?( (@y-@a) , (@y+@width+@a) )   || 
+          (pacmanY+31).between?( (@y-@a), (@y+@width+@a) ) ||
+          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) ) 
+        return true
+      end
     else
       return false
     end
   end
 
   def hitRight(pacmanX , pacmanY)
-    if  ((pacmanX + 2) == @x+@height) &&
-        ( (pacmanY).between?( (@y-@a) , (@y+@width+@a) )   || 
-          (pacmanY+40).between?( (@y-@a), (@y+@width+@a) ) ||
-          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) ) &&
-        (@through == false)
-      return true
+    if (@through == false)
+      if  ((pacmanX + 5 ) == @x+@height) &&
+        ( (pacmanY+9).between?( (@y-@a) , (@y+@width+@a) )   || 
+          (pacmanY+31).between?( (@y-@a), (@y+@width+@a) ) ||
+          (pacmanY+20).between?( (@y-@a) , (@y+@width+@a) ) )   
+        return true
+      end
     else
       return false
     end
